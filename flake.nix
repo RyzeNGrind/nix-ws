3a6ba7f179c14d14d686c0734377eec5948bdfbe
RyzeNGrind <ryzengrind@users.noreply.github.com>
2025-04-17 22:44:39 +0000

    Basic flake

--- flake.nix
+++ flake.nix
@@ -1,32 +1,44 @@
 {
+  description = "NixOS system configuration with clustering support";
+
   inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
     flake-parts.url = "github:hercules-ci/flake-parts";
     divnix-std.url = "github:divnix/std";
     home-manager.url = "github:nix-community/home-manager";
   };
 
-  outputs = { self, inputs } @ attrs: {
-    nixosConfigurations = { };
+  outputs = inputs @ { flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
+      
+      imports = [
+        ./modules/base-system.nix
+        ./modules/impermanence.nix
+        ./modules/home-config.nix
+      ];
 
-    modules = [
-      ./modules/base-system.nix
-      ./modules/impermanence.nix
-      ./modules/home-config.nix
-    ];
+      perSystem = { config, self', inputs', pkgs, system, ... }: {
+        # System-specific configurations can go here
+      };
 
-    clusterConfig = inputs.flake-parts.lib.makeModule {
-      imports = [ ./clusters ];
+      flake = {
+        nixosConfigurations = { };
+
+        clusterConfig = inputs.flake-parts.lib.makeModule {
+          imports = [ ./clusters ];
+        };
+
+        nodeRoles = {
+          workstation = ./roles/workstation.nix;
+          server = ./roles/server.nix;
+          edge = ./roles/edge.nix;
+        };
+
+        cloudProviders = {
+          oci = ./providers/oracle.nix;
+          lxd = ./providers/lxd.nix;
+        };
+      };
     };
-
-    nodeRoles = {
-      workstation = ./roles/workstation.nix;
-      server = ./roles/server.nix;
-      edge = ./roles/edge.nix;
-    };
-
-    cloudProviders = {
-      oci = ./providers/oracle.nix;
-      lxd = ./providers/lxd.nix;
-    };
-  };
 }
